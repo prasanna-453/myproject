9640c13e4e3bf1d5465541ad53a50be51e7158ec
prsanna-453 <dunukulaprasanna@gmail.com>
2024-11-14 10:23:53 +0530

    exceptions

--- src/main/java/com/neoteric/exception/ExceptionTest.java
+++ src/main/java/com/neoteric/exception/ExceptionTest.java
@@ -3,8 +3,6 @@ package com.neoteric.exception;
 public class ExceptionTest {
 
     public void calculateWithFinally(int numberOfTickets)throws Exception{
-        try {
-            System.out.println("available"+);
-        }
+
     }
 }
